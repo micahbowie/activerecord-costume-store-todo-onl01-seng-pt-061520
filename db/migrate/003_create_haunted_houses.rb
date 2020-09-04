d488e41ec2ecb4fa707193e4ca94f53a491100f5
Micah Bowie <micahbowie20@gmail.com>
2020-09-04 22:24:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateCostumeStore < ActiveRecord::Migration[5.2]
+ def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.string :still_in_business
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
+end
