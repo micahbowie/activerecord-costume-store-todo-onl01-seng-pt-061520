b79e306403ff959dc7d9b9fab7a76ee09743dce1
Micah Bowie <micahbowie20@gmail.com>
2020-09-04 22:18:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,12 @@ class CreateCostumeStore < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.decimal :location
-      t.string :size
-      t.string :image_url
+      t.string :costume_inventory
+      t.string :number_of_employees
+      t.string :in_business_tf
+      t.decimal :location
+      t.string :opening_time
+      t.string :closing_time
     end
   end
 end
