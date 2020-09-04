0bc467a1d209ba402a71427abd80460ecb2b5573
Micah Bowie <micahbowie20@gmail.com>
2020-09-04 22:19:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStore < ActiveRecord::Migration[5.2]
  def change
     create_table :costumes do |t|
       t.string :name
-      t.decimal :location
-      t.string :costume_inventory
+      t.string :location
+      t.integer :costume_inventory
       t.string :number_of_employees
       t.string :in_business_tf
       t.decimal :location
