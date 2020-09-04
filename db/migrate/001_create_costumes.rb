baa580f238319f61759ff0cef9fb26cf28823d9f
Micah Bowie <micahbowie20@gmail.com>
2020-09-04 22:17:34 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.decimal :price
       t.string :size
       t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
